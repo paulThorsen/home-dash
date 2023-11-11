bca7dc7184b47c9c95f7b04cb9943acb6fc5f04d
Paul Thorsen <paul_thorsen@byu.edu>
2023-11-10 18:10:31 -0700

    transition to tile for button

--- src/app/gadgets/garage/garage.component.ts
+++ src/app/gadgets/garage/garage.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MerossService } from '../../services/meross.service';
 import { GarageService } from '../../interfaces/garage-service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { TileComponent } from '../../ui/components/tile/tile.component';
 
 @Component({
   selector: 'app-garage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TileComponent],
   templateUrl: './garage.component.html',
   styleUrl: './garage.component.scss',
 })
