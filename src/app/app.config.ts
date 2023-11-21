ea599fc90e93c825675b85a947b3d75d1b15ef83
Paul Thorsen <paul_thorsen@byu.edu>
2023-11-20 19:53:12 -0700

    cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
-  ],
+  providers: [provideRouter(routes), provideHttpClient()],
 };
