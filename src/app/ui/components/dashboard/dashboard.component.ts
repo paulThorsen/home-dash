cb234fe555dcf0dbd3e7700a78d59db24c1308f5
Paul Thorsen <paul_thorsen@byu.edu>
2024-01-03 16:00:41 -0700

    add simple forecast

--- src/app/ui/components/dashboard/dashboard.component.ts
+++ src/app/ui/components/dashboard/dashboard.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { TileComponent } from '../tile/tile.component';
 import { GarageComponent } from '../../../gadgets/garage/garage.component';
 import { TVComponent } from '../../../gadgets/tv/tv.component';
+import { ForecastComponent } from '../../../forecast/forecast.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, TileComponent, GarageComponent, TVComponent],
+  imports: [
+    CommonModule,
+    TileComponent,
+    GarageComponent,
+    TVComponent,
+    ForecastComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
